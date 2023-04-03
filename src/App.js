d12cca98997e02950b7fabc8e95e3b246dc96400
Aleksey <aa.gorschenko@yandex.ru>
2023-04-03 19:20:48 +0300

    DefaultButton has been added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import DefaultButton from 'components/base/DefaultButton/DefaultButton.js'
 
 function App() {
   return (
     <div className="App">
-
+      <DefaultButton
+        title="Some title"
+      />
     </div>
   );
 }
--- src/components/base/DefaultButton/DefaultButton.js
+++ src/components/base/DefaultButton/DefaultButton.js
@@ -1,12 +1,29 @@
 import React from 'react'
-import '@/components/base/DefaultButton/DefaultButton.scss'
+import PropTypes from 'prop-types'; 
+import 'components/base/DefaultButton/DefaultButton.scss'
 
 export default class DefaultButton extends React.Component {
+
     render() {
         return (
-            <button>
-                
+            <button className={`
+                default-button
+                default-button_${this.props.size}
+                default-button_${this.props.color}
+            `}>
+                {this.props.title}
             </button>
         )
     }
+}
+
+DefaultButton.propTypes = {
+    title: PropTypes.string.isRequired,
+    size: PropTypes.string,
+    color: PropTypes.string,
+}
+
+DefaultButton.defaultProps = {
+    size: 'm',
+    color: 'primary',
 }
