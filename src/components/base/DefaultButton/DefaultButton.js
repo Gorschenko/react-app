fd5da34d2cf260a2ab60cb5d6b4c319ff2079799
Aleksey <aa.gorschenko@yandex.ru>
2023-04-03 18:45:06 +0300

    Some files have been removed

---
+++ src/components/base/DefaultButton/DefaultButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '@/components/base/DefaultButton/DefaultButton.scss'
+
+export default class DefaultButton extends React.Component {
+    render() {
+        return (
+            <button>
+                
+            </button>
+        )
+    }
+}
