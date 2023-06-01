04807c1d5e5a28a9ddf5860beab7b4e7672c6938
Aleksey <aa.gorschenko@yandex.ru>
2023-06-01 16:46:00 +0300

    Func for password field has been edited

--- src/components/base/textField.jsx
+++ src/components/base/textField.jsx
@@ -1,19 +1,34 @@
+import { useState } from "react";
+
 const TextField = ({ label, type, name, onChange, value, error }) => {
+  const [showPassword, setShowPassword] = useState();
   const getInputClasses = () => {
     return "form-control is-" + (error ? "invalid" : "valid");
   };
+  const toggleShowPassword = () => setShowPassword((prevState) => !prevState);
   return (
     <div className="mb-4">
       <label htmlFor={name}>{label}</label>
-      <input
-        type={type}
-        id={name}
-        name={name}
-        value={value}
-        onChange={onChange}
-        className={getInputClasses()}
-      />
-      {error && <div className="invalid-feedback">{error}</div>}
+      <div className="input-group has-validation">
+        <input
+          type={showPassword ? "text" : type}
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+          className={getInputClasses()}
+        />
+        {type === "password" && (
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            onClick={toggleShowPassword}
+          >
+            Показать
+          </button>
+        )}
+        {error && <div className="invalid-feedback">{error}</div>}
+      </div>
     </div>
   );
 };
