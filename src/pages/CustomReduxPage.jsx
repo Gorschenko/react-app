7643c035e660f3a32dda5bffb6776afaabf7b75c
Aleksey <aa.gorschenko@yandex.ru>
2023-06-06 18:56:43 +0300

    Reducer has been added

--- src/pages/CustomReduxPage.jsx
+++ src/pages/CustomReduxPage.jsx
@@ -1,31 +1,66 @@
 import * as React from "react";
 
-const createStore = (initialState) => {
-    let state = initialState
-
-    const getState = () => state
-    const dispatch = (action) => {
-        if (action.type === 'task/completed') {
-            const newArray = [...state]
-            const elementIndex = newArray.findIndex(el => el.id === action.payload.id)
-            newArray[elementIndex].completed = true
-            state = newArray
-            console.log(state)
-        }
+const taskReducer = (state, action) => {
+  switch (action.type) {
+    case "task/completed": {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      newArray[elementIndex].completed = true;
+      return newArray;
     }
+    default:
+      break;
+  }
+};
 
-    return {getState, dispatch}
-}
+const createStore = (reducer, initialState) => {
+  let state = initialState;
 
-const store = createStore([{id: 1, description: 'Task 1', completed: false}])
+  const getState = () => state;
+  const dispatch = (action) => {
+    state = reducer(state, action);
+  };
+
+  return { getState, dispatch };
+};
+
+const store = createStore(taskReducer, [
+  { id: 1, description: "Task 1", completed: false },
+  { id: 2, description: "Task 2", completed: false },
+]);
 
 const CustomReduxPage = () => {
-    console.log(store.getState())
-   
-    return <>
-        <h1>Redux</h1>
-        <button className="btn btn-primary" onClick={() =>  store.dispatch({type: 'task/completed', payload: { id: 1 }})}>Complete</button>
+  const state = store.getState();
+  console.log(state);
+
+  const completeTask = (taskId) => {
+    store.dispatch({ type: "task/completed", payload: { id: taskId } });
+    console.log(store.getState());
+  };
+
+  return (
+    <>
+      <h1>Redux</h1>
+
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.description}</p>
+            <p>{`Completed: ${el.completed}`}</p>
+            <button
+              className="btn btn-primary"
+              onClick={() => completeTask(el.id)}
+            >
+              Complete
+            </button>
+            <hr />
+          </li>
+        ))}
+      </ul>
     </>
+  );
 };
 
 export default CustomReduxPage;
