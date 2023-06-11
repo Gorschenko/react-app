6e45d816f4f529eff899bcf693a40dd1975460ae
Alexey <aa.gorschenko@yandex.ru>
2023-06-11 13:40:05 +0300

    createSlice for tasks has been added

--- src/store/store.js
+++ src/store/store.js
@@ -4,5 +4,5 @@
 
 
  export default function configureStore() {
-    return createStore(taskReducer)
+    return createStore(taskReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
  }
