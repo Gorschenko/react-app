1c96d2d54a4f2700ac608356d5c11cfe63b270b1
Alexey <aa.gorschenko@yandex.ru>
2023-06-11 13:19:09 +0300

    React Toolkit has been initiakized

--- src/store/task.js
+++ src/store/task.js
@@ -1,47 +1,37 @@
-const TASK_UPDATED = "task/updated";
-const TASK_DELETED = "task/deleted";
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
+const initialState = [
+  { id: 1, title: "Task 1", completed: false },
+  { id: 2, title: "Task 2", completed: false },
+];
+
+const update = createAction('task/updated');
+const remove = createAction('task/removed');
 
 export function taskCompleted(id) {
-    return {
-        type: TASK_UPDATED,
-        payload: { id, completed: true },
-      }
+  return update({ id, completed: true })
  }
 
  export function titleChanged(id) {
-    return { 
-        type: TASK_UPDATED,
-        payload: { id, title: `New title for ${id}` },
-      }
+  return update({ id, title: `New title for ${id}` })
  }
 
  export function taskDeleted(id) {
-    return {
-        type: TASK_DELETED,
-        payload: { id },
-    }
+  return remove({ id })
  }
 
-
- const taskReducer = (state = [], action) => {
-   switch (action.type) {
-     case TASK_UPDATED: {
-       const newArray = [...state];
-       const elementIndex = newArray.findIndex(
-         (el) => el.id === action.payload.id
-       );
-       newArray[elementIndex] = { ...newArray[elementIndex], ...action.payload };
-       return newArray;
-     }
-     case TASK_DELETED: {
-       const newArray = [...state].filter(i => i.id !== action.payload.id);
-       
-       return newArray;
-     }
-     default:
-       return state
-   }
- };
+const taskReducer = createReducer(initialState, (builder) => {
+  builder
+  .addCase(update, (state, action) => {
+    const elementIndex = state.findIndex(
+      (el) => el.id === action.payload.id
+    );
+    state[elementIndex] = { ...state[elementIndex], ...action.payload };
+  })
+  .addCase(remove, (state, action) => {
+    return state.filter(i => i.id !== action.payload.id)
+  })
+})
  
  export  default taskReducer;
  
